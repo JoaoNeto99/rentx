7424b665afca0cbee1558b147fa3e71954185bc2
Joao Lopes Santana Neto <joaoneto7499@gmail.com>
2023-01-13 14:15:16 -0300

    Building header

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,37 @@
 import React from "react";
-import {Container,  Title} from "./styles";
+import {StatusBar} from "react-native";
+
+import {
+    Container,
+    Header,
+    TotalCars,
+    HeaderContent
+} from "./styles";
+
+import Logo from "../../assets/logo.svg"
+import {RFValue} from "react-native-responsive-fontsize";
+import {Car} from "../../components/Car";
 
 export function Home() {
     return (
         <Container>
+            <StatusBar
+                barStyle={"light-content"}
+                translucent={true}
+                backgroundColor={"transparent"}/>
+            <Header>
+                <HeaderContent>
+                    <Logo
+                        width={RFValue(108)}
+                        height={RFValue(12)}
+                    />
+                    <TotalCars>
+                        Total de 12 carros
+                    </TotalCars>
+                </HeaderContent>
+
+            </Header>
+            <Car  />
 
         </Container>
     )
